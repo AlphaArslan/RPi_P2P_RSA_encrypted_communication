f8acc8ec4c7b375b460c94741a729d6f79bed0c2
AlphaArslan <arslan.alpha21@gmail.com>
2019-02-16 09:38:07 +0200

    one way - plain text

--- peer1.py
+++ peer1.py
@@ -1,19 +1,33 @@
+################### IMPORT
 import socket
 
-#setup socket
+################### Global Variables
 my_ip = "192.168.1.5"
 his_ip = "192.168.1.6"
 port = 2124
+msg = ""
 s = socket.socket()
+
+################### Function
+def getpublickey():
+    pubKey = tuple(map(int,input("Enter publicKey q, n : ").split(',')))
+    return pubKey
+
+def RSAencrypt(data):
+    return data
+
+################### SETUP
 s.bind((my_ip,port))
 s.connect((his_ip, port))
-msg = ""
+
+################### Main
+
+
 while True:
     #get text from user and encrypt it
     msg = input("-> ")
     s.sendall(msg.encode('utf-8'))
 
-#send it to peer2
 
 s.close()
 s.detach()
--- peer2.py
+++ peer2.py
@@ -1,16 +1,27 @@
+################### IMPORT
 import socket
 
+################### Global Variables
 my_ip  = "192.168.1.6"
 his_ip = "192.168.1.5"
 port   = 2124
-
 s = socket.socket()
+
+################### Function
+def getprivatekey():
+    privateKey = tuple(map(int,input("Enter publicKey q, n : ").split(',')))
+    return privateKey
+
+def RSA_decrypt(text)
+	return text
+
+################### Setup
 s.bind((my_ip, port))
 s.listen(1)
-
 conn, addr = s.accept()
-
 print "connected to : ", addr
+
+################### Main
 
 while 1:
 	data = conn.recv(1024)
